82cb5ef76e8f84ff1865f88f09b7143c864afcb2
Clark Gaebel <cg.wowus.cg@gmail.com>
2020-11-14 18:15:43 -0500

    view stub

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 mod cmdline;
 mod editor;
 
+use anyhow::Result;
 use cmdline::Opts;
+use std::fs;
 use std::path::PathBuf;
+use std::str;
 
-fn view(file: &PathBuf) {
-    println!("viewing {:?}", file);
+fn view(file: &PathBuf) -> Result<()> {
+    let binary_file_contents = fs::read(file)?;
+    let str_file_contents = str::from_utf8(&binary_file_contents)?;
+    print!("{}", str_file_contents);
+    Ok(())
 }
 
-fn edit(file: &PathBuf) {
+fn edit(file: &PathBuf) -> Result<()> {
     println!("editing {:?}", file);
+    Ok(())
 }
 
 fn main() {
     let what_to_do = cmdline::parse();
-    match what_to_do {
+    let result = match what_to_do {
         Opts::View { file } => view(&file),
         Opts::Edit { file } => edit(&file),
-    }
+    };
+    result.unwrap()
 }
